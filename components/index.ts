79501f88e6a63d12a4ffa8dc9640a008c7a786a2
YohanNayanajith <yohannayanajith40@gmail.com>
2023-09-04 14:47:31 +0530

    task/ add companies

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,12 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
 import Hero from "./Hero";
+import JoinedCompanies from "./JoinedCompanies";
 
 export {
     Footer,
     Navbar,
     CustomButton,
-    Hero
+    Hero,
+    JoinedCompanies
 }

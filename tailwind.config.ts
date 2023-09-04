59ed442581f8878b2318c73ec5cce559c2364580
YohanNayanajith <yohannayanajith40@gmail.com>
2023-09-04 14:52:33 +0530

    bug/cannot find hero-bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,7 +31,7 @@ module.exports = {
       },
       backgroundImage: {
         pattern: "url('./pattern.png')",
-        "hero-bg": "url('./hero-bg.png')",
+        "hero-bg": "url('/hero-bg.png')",
       },
     },
   },
